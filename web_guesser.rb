f8d53f33283a8628abf381ca2ebfc1aa069a6672
Max Glassie <msglassie@gmail.com>
2017-01-20 12:05:56 -0700

    completes tutorial with coloring extension

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,9 +1,35 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+def initialize
+  @color = nil
+end
 
-x = rand(100)
+SECRET_NUMBER = rand(100)
+
+def check_guess(guess)
+  if (guess - 5) > SECRET_NUMBER
+    @color = "background: red"
+    "Way too high!"
+  elsif (guess + 5) < SECRET_NUMBER
+    @color = "background: pink"
+    "Way too low!"
+  elsif guess > SECRET_NUMBER
+    @color = "background: pink"
+    "Too high!"
+  elsif guess < SECRET_NUMBER
+    @color = "background: pink"
+    "Too low!"
+  else
+    @color = "background: green"
+    "You got it right! The SECRET NUMBER is #{SECRET_NUMBER}"
+  end
+end
+
 
 get '/' do
-  "The SECRET NUMBER is #{x}"
+  guess = params['guess'].to_i
+  message = check_guess(guess)
+  color = @color
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :color => @color}
 end
